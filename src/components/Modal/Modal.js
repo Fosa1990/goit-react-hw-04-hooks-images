9a5257c1e8dd7cc525536a939fa57fa3f8351cbd
Fosa1990 <fosa1990@gmail.com>
2021-12-18 14:36:28 +0200

    rewritten Modal into hooks

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import IconButton from '../IconButton';
 import styles from './Modal.module.css';
@@ -7,47 +7,40 @@ import { ReactComponent as CloseIcon } from '../../images/svg/close.svg';
 
 const modalRoot = document.querySelector('#root-modal');
 
-export class Modal extends Component {
-  static propTypes = {
-    onClose: PropTypes.func.isRequired,
-    children: PropTypes.node,
+export default function Modal({ onClose, children }) {
+  useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.code === 'Escape') onClose();
+    }
+    window.addEventListener('keydown', handleKeyDown);
+
+    return function onClearup() {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+
+  const handleBackdropClick = event => {
+    if (event.target === event.currentTarget) onClose();
   };
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = event => {
-    if (event.code === 'Escape') this.props.onClose();
-  };
-
-  handleBackdropClick = event => {
-    if (event.target === event.currentTarget) this.props.onClose();
-  };
-
-  render() {
-    const { onClose, children } = this.props;
-
-    return createPortal(
-      <div className={styles.Backdrop} onClick={this.handleBackdropClick}>
-        <div className={styles.Modal}>
-          <IconButton
-            onClick={onClose}
-            className={styles.CloseModal}
-            aria-label="Close Modal Button"
-          >
-            <CloseIcon width="32" height="32" />
-          </IconButton>
-          {children}
-        </div>
-      </div>,
-      modalRoot,
-    );
-  }
+  return createPortal(
+    <div className={styles.Backdrop} onClick={handleBackdropClick}>
+      <div className={styles.Modal}>
+        <IconButton
+          onClick={onClose}
+          className={styles.CloseModal}
+          aria-label="Close Modal Button"
+        >
+          <CloseIcon width="32" height="32" />
+        </IconButton>
+        {children}
+      </div>
+    </div>,
+    modalRoot,
+  );
 }
 
-export default Modal;
+Modal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.node,
+};
